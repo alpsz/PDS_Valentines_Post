66a606292b18d72278c78be91f553477bf8b03a2
unknown <alpesh200720@gmail.com>
2022-05-08 22:17:41 -0400

    create repositoty and run npm start

---
+++ js/script.js
@@ -0,0 +1,31 @@
+﻿
+let slider = tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    controlsContainer: '#customize-controls',
+    prevButton: '.prev',
+    nextButton: '.next',
+    autoplayButton: '.auto',
+    navPosition: 'bottom',
+    responsive: {
+        1600: {
+            items: 3,
+            gutter: 20,
+            nav: true
+        },
+        1024: {
+            items: 3,
+            gutter: 20,
+            nav: true
+        },
+        768: {
+            items: 2,
+            gutter: 20,
+            nav: true
+        },
+        480: {
+            items: 1
+        }
+    }
+});
